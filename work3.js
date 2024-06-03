7b0ca0510b2f4ada4a7d9bf7d7c5a56d34ca0a89
Saccharin01 <chousik01@gmail.com>
2024-06-03 12:06:40 +0900

    function Moduleize

--- work3.js
+++ work3.js
@@ -1,12 +1,10 @@
 function workThree(index, name) {
   let local = {
-    Number : index+1,
+    Number : index + 1,
     name : name
   };
 
   return local;
 };
 
-let check = workThree(0, "alpha")
-console.log(check)
-
+module.exports = workThree;
