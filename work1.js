8433778cb3bf7785abe0256e2dca84f62e53d0f0
Saccharin01 <chousik01@gmail.com>
2024-06-03 11:52:48 +0900

    work done

--- work1.js
+++ work1.js
@@ -8,13 +8,23 @@ function workOne(a, b){
    * * logic comment 
    * *매개변수 두 개를 받은 다음, 리턴은 객체
    * * 매개변수 a, b가 객체의 key, value가 되었으면 좋겠다
+   * * Condition : 매개변수가 모두 "문자열"이여야 함ㅁ
    * */
-  let localVariable = {
-  };
-  localVariable[a] = a;
-  localVariable[b] = b;
-
-  return localVariable;
+  if(typeof(a)==="string"){
+    if(typeof(b)==="string"){
+      let localVariable = {
+      };
+      localVariable[a] = a;
+      localVariable[b] = b;
+    
+      return localVariable;
+    }
+    else{
+      console.error("B Argument Type is not String")
+    }
+  }else{
+    console.error("A Argument Type is not String")
+  }
 } 
-console.log(workOne(`alpha`, `beta`))
+console.log(workOne("1", 2))
 workOne()
